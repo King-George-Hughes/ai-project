caecaa7006388cd8c045bd422f5a8dc49b824fac
unknown <hughesgeorgeking@gmail.com>
2023-07-25 22:34:14 +0000

    changes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboadPage() {
+  return <p>Dashboard Page (Protected)</p>;
+}
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <p className="text-6xl text-green-700">Hello AI-SAAS</p>;
-}
