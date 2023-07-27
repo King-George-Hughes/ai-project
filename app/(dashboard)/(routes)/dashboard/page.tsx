3342bab782357db562a170c57ddda7bdf9e6a6b5
unknown <hughesgeorgeking@gmail.com>
2023-07-27 21:15:36 +0000

    changes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { Card } from "@/components/ui/card";
-import { MessageSquare } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  ArrowRight,
+  MessageSquare,
+  Music,
+  ImageIcon,
+  VideoIcon,
+  Code,
+} from "lucide-react";
 
 const tools = [
   {
@@ -11,9 +20,39 @@ const tools = [
     bgColor: "bg-violet-500/10",
     href: "/conversation",
   },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music-generation",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image-generation",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-500",
+    bgColor: "bg-orange-500/10",
+    href: "/video-generation",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code-generation",
+  },
 ];
 
 export default function DashboadPage() {
+  const router = useRouter();
+
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -29,9 +68,20 @@ export default function DashboadPage() {
         {tools.map((tool) => {
           return (
             <Card
+              onClick={() => router.push(tool.href)}
               key={tool.href}
               className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
-            ></Card>
+            >
+              <div className="flex items-center gap-4">
+                <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                  <tool.icon className={cn("w-8 h-8", tool.color)} />
+                </div>
+
+                <div className="font-semibold">{tool.label}</div>
+              </div>
+
+              <ArrowRight className="w-5 h-5" />
+            </Card>
           );
         })}
       </div>
